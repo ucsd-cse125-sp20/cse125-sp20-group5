41c1af1a7fd4be6a602a793a8e21069d3162bca9
EtsuN <etnakaha@ucsd.edu>
2020-04-23 08:34:12 +0800

    A little code cleaning on AssimpModel

--- game_client/AssimpModel.cpp
+++ game_client/AssimpModel.cpp
@@ -286,8 +286,7 @@ void AssimpModel::ReadNodeHeirarchy(float AnimationTime, const aiNode* pNode, co
 
 		aiVector3D Translation;
 		CalcInterpolatedPosition(Translation, AnimationTime, pNodeAnim);
-		glm::mat4 TranslationM = glm::translate(glm::vec3(Translation.x, Translation.y, Translation.z));// potential error place TODO
-		std::cout << Translation.x << " " << Translation.y << " " << Translation.z << std::endl;
+		glm::mat4 TranslationM = glm::translate(glm::vec3(Translation.x, Translation.y, Translation.z));
 
 		// Combine the above transformations
 		NodeTransformation = TranslationM * RotationM * ScalingM;
@@ -332,7 +331,7 @@ void AssimpModel::CalcInterpolatedPosition(aiVector3D& Out, float AnimationTime,
 	uint NextPositionIndex = (PositionIndex + 1);
 	assert(NextPositionIndex < pNodeAnim->mNumPositionKeys);
 	float DeltaTime = (float)(pNodeAnim->mPositionKeys[NextPositionIndex].mTime - pNodeAnim->mPositionKeys[PositionIndex].mTime);
-	float Factor = (AnimationTime - (float)pNodeAnim->mPositionKeys[PositionIndex].mTime) / DeltaTime;
+	float Factor = (AnimationTime - (float)pNodeAnim->mPositionKeys[PositionIndex].mTime) / DeltaTime; //TODO this assert fails
 	assert(Factor >= 0.0f && Factor <= 1.0f);
 	const aiVector3D& Start = pNodeAnim->mPositionKeys[PositionIndex].mValue;
 	const aiVector3D& End = pNodeAnim->mPositionKeys[NextPositionIndex].mValue;
@@ -434,7 +433,7 @@ glm::mat4 convertToGlmMat(const aiMatrix4x4& AssimpMatrix) {
 	m[1][0] = AssimpMatrix.b1; m[1][1] = AssimpMatrix.b2; m[1][2] = AssimpMatrix.b3; m[1][3] = AssimpMatrix.b4;
 	m[2][0] = AssimpMatrix.c1; m[2][1] = AssimpMatrix.c2; m[2][2] = AssimpMatrix.c3; m[2][3] = AssimpMatrix.c4;
 	m[3][0] = AssimpMatrix.d1; m[3][1] = AssimpMatrix.d2; m[3][2] = AssimpMatrix.d3; m[3][3] = AssimpMatrix.d4;
-	return glm::transpose(m);
+	return glm::transpose(m); // since aiMatrix4x4 treats glm::mat4's rows as columns
 }
 
 unsigned int AssimpModel::textureFromFile(const char* path, const string& directory, bool gamma)
