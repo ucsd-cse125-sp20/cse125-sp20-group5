d693967c685fb8f0af19ec5448da76c32d385ff9
arunsug <arunsug@gmail.com>
2020-05-21 11:24:14 -0700

    packages are in I think

--- game_client/main.cpp
+++ game_client/main.cpp
@@ -1,6 +1,11 @@
 #include "core.h"
 #include "Client.h"
 #include <iostream>
+#include <nanogui/glutil.h>
+#include <nanogui/button.h>
+#include <nanogui/window.h>
+#include <nanogui/screen.h>
+
 
 // These are really HACKS to make glfw call member functions instead of static functions
 
@@ -82,6 +87,17 @@ int main(int argc, char** argv) {
 	glfwSetCursorPosCallback(windowHandle, smouseMotion);
 	glfwSetWindowSizeCallback(windowHandle, sresize);
 	glfwSetScrollCallback(windowHandle, sscroll);
+
+
+	nanogui::Window* window = new nanogui::Window(this, "Button demo");
+	window->setPosition(nanogui::Vector2i(15, 15));
+	window->setLayout(new nanogui::GroupLayout());
+
+	/* No need to store a pointer, the data structure will be automatically
+	   freed when the parent window is deleted */
+	new Label(window, "Push buttons", "sans-bold");
+	nanogui::Button* b = new nanogui::Button(windowHandle, "Plain button");
+	b->setCallback([] { cout << "pushed!" << endl; });
 
 
 	// Initialize GLEW
