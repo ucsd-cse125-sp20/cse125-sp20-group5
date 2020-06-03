e8c8ae5d662c9484aa068ee35dcd2ee9837d3163
arunsug <arunsug@gmail.com>
2020-06-03 15:15:49 -0700

    grid

--- game_client/ChooseLevel.cpp
+++ game_client/ChooseLevel.cpp
@@ -1,13 +1,24 @@
 #include "ChooseLevel.h"
 
+
 ChooseLevel::ChooseLevel(GLFWwindow* window, nanogui::Screen* screen, uint numLevels)
 {
 	numPlayers = 1;
 	this->screen = screen;
 	nanogui::Window* nanoguiWindow = new nanogui::Window(screen, "Choose Level");
 	wind = nanoguiWindow;
-	nanoguiWindow->setFixedSize(nanogui::Vector2i(200, 500));
-	nanoguiWindow->setLayout(new nanogui::GroupLayout());
+	nanoguiWindow->setFixedSize(nanogui::Vector2i(900, 500));
+	nanoguiWindow->setPosition(nanogui::Vector2i(30, 30));
+
+	nanogui::GridLayout* layout =
+		new nanogui::GridLayout(nanogui::Orientation::Vertical, 2,
+			nanogui::Alignment::Middle, 5, 5);
+	layout->setColAlignment(
+		{ nanogui::Alignment::Maximum, nanogui::Alignment::Fill });
+	layout->setSpacing(0, 10);
+	nanoguiWindow->setLayout(layout);
+
+	nanogui::Label* numLabel = new nanogui::Label(nanoguiWindow, std::string("Players In Lobby"));
 
 	nanogui::TextBox* textBox = new nanogui::TextBox(nanoguiWindow, "1");
 	textBox->setEditable(false);
@@ -18,14 +29,19 @@ ChooseLevel::ChooseLevel(GLFWwindow* window, nanogui::Screen* screen, uint numLe
 	textBox->setAlignment(nanogui::TextBox::Alignment::Center);
 	text = textBox;
 
+	images = new GLuint[numLevels];
 
 	for (uint i = 0; i < numLevels; i++) {
 		nanogui::Button* level = new nanogui::Button(nanoguiWindow, std::string("Level ") + std::to_string(i));
 		level->setCallback([textBox]() { std::cout << "Button pressed. " << textBox->value() << std::endl; });
+
+		/*level = new nanogui::Button(nanoguiWindow, std::string("Level ") + std::to_string(i));
+		level->setCallback([textBox]() { std::cout << "Button pressed. " << textBox->value() << std::endl; });*/
+		readInImages(images + i, "texture/ground_texture.jpg");
+		nanogui::ImageView* view = new nanogui::ImageView(nanoguiWindow, images[i]);
+		view->setFixedSize(nanogui::Vector2i(100, 100));
 		buttons.push_back(level);
 	}
-
-
 
 	screen->setVisible(true);
 	screen->performLayout();
@@ -52,4 +68,28 @@ void ChooseLevel::setPlayers(uint numPlayers)
 {
 	this->numPlayers = numPlayers;
 	text->setValue(std::to_string(numPlayers));
+}
+
+void ChooseLevel::readInImages(GLuint * imageID, const char * texturefile) {
+	//store texture buffer
+	glBindTexture(GL_TEXTURE_2D, *imageID);
+	// set the texture wrapping/filtering options (on the currently bound texture object)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// load and generate the texture
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load(texturefile, &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		//glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture " << texturefile << std::endl;
+	}
+	stbi_image_free(data);
 }
--- game_client/ChooseLevel.h
+++ game_client/ChooseLevel.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "core.h"
+#include "stb_image.h"
 #include <nanogui/button.h>
 #include <nanogui/window.h>
 #include <nanogui/screen.h>
 #include <nanogui/layout.h>
 #include <nanogui/label.h>
 #include <nanogui/textbox.h>
+#include <nanogui/ImageView.h>
 
 class ChooseLevel
 {
@@ -14,11 +16,13 @@ public:
 	int getLevel();
 	void removeWindow();
 	void setPlayers(uint numPlayers);
+	void readInImages(GLuint* imageID, const char* texturefile);
 
 private:
 	uint numPlayers;
 
 	std::vector<nanogui::Button *> buttons;
+	GLuint * images;
 	nanogui::Screen * screen;
 	nanogui::Window * wind;
 	nanogui::TextBox * text;
